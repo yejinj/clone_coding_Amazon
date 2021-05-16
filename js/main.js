584cda2cae2f5a9ce02365fc6ab11d51db11b8aa
yejinj <80540445+yejinj@users.noreply.github.com>
2021-05-17 00:01:06 +0900

    commit to main

--- js/main.js
+++ js/main.js
@@ -1,6 +1,31 @@
 // 슬라이드를 사용하기 위해 myswiper을 초기화한다
 new Swiper('.myswiper', {
-
-	// 슬라이드 속도 설정, 아무것도 없을 시 기본값은 300
-	speed : 500,
+	infinite : true, 
+	autoplay : true,
+	autoplaySpeed: 5000,
+	navigation : {
+		nextEl : '.swiper-button-next', // 다음 버튼 클래스명
+		prevEl : '.swiper-button-prev', // 이번 버튼 클래스명
+	},
 });
+
+var btn = document.getElementById('input-group-addon-button');
+
+btn.onclick = function() {
+  alert('사이트에 방문해보세요');
+}
+
+var mouseover = document.getElementById('whereto');
+
+mouseover.mouseover = function() {
+}
+
+// mysidebar
+
+function openNav() {
+	document.getElementById("mySidenav").style.width = "300px";
+}
+
+function closeNav() {
+	document.getElementById("mySidenav").style.width = "0";
+}
