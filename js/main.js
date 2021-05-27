5460b3e8994132e9d92234b9ef9a3cfda7b05648
yejinj <80540445+yejinj@users.noreply.github.com>
2021-05-27 17:49:19 +0900

    c t m

--- js/main.js
+++ js/main.js
@@ -39,3 +39,25 @@ $backToTop.addEventListener("click", function () {
 	  behavior: "smooth",
 	});
   });
+
+//   첨가
+function view(opt) {
+	if(opt) {
+	   hidden_div.style.display = "block";
+	}
+	else {
+		hidden_div.style.display = "none";
+	}
+  }
+
+// cj
+
+// html dom 이 다 로딩된 후 실행된다.
+$(document).ready(function(){
+	// memu 클래스 바로 하위에 있는 a 태그를 클릭했을때
+	$(".menu>a").click(function(){
+		// 현재 클릭한 태그가 a 이기 때문에
+		// a 옆의 태그중 ul 태그에 hide 클래스 태그를 넣던지 빼던지 한다.
+		$(this).next("ul").toggleClass("hide");
+	});
+});
